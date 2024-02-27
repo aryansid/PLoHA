fd0e8e67e7e361e9addbdd1ff164a429366b5713
Aryan Siddiqui <aryansid@stanford.edu>
2024-02-26 19:32:34 -0800

    first commit

---
+++ data_utils.py
@@ -0,0 +1,104 @@
+from datasets import load_dataset
+
+class DatasetLoader(object):
+    def __init__(self, dataset_name, source_dataset_name, dataset_version, has_valid, split_map,
+                 batch_size, train_batch_idxs, test_batch_idxs, valid_batch_idxs=None):
+        self.data_root = DATASET_ROOT
+        self.dataset_name = dataset_name
+        self.source_dataset_name = source_dataset_name
+        self.dataset_version = dataset_version
+        self.has_valid = has_valid
+        self.split_map = split_map
+
+        self.batch_size = batch_size
+        self.train_batch_idxs = train_batch_idxs
+        self.test_batch_idxs = test_batch_idxs
+        self.valid_batch_idxs = valid_batch_idxs
+        
+        assert self.split_map is not None    
+
+
+    def load_from_source(self):
+        if self.source_dataset_name is None:
+            self.source_dataset_name = self.dataset_name
+        if self.dataset_version is None:
+            datasets = load_dataset(self.source_dataset_name)
+        else:
+            datasets = load_dataset(self.source_dataset_name, self.dataset_version)
+        return datasets
+
+
+    def to_json(self, datasets):
+        for k, v in self.split_map.items():
+            datasets[v].to_json(f'{self.data_root}/{self.dataset_name}/{self.dataset_name}_{k}.json')
+
+
+    def load_from_json(self):
+        data_files = {
+            'train': f'{self.data_root}/{self.dataset_name}/{self.dataset_name}_train.json',
+            'test': f'{self.data_root}/{self.dataset_name}/{self.dataset_name}_test.json',
+        }
+
+        if self.has_valid:
+            data_files.update({'valid': f'{self.data_root}/{self.dataset_name}/{self.dataset_name}_valid.json',})
+
+        datasets = load_dataset('json', data_files=data_files)
+        datasets = self._post_process(datasets) 
+
+        # subsample training dataset if needed
+        num_train = len(datasets['train'])
+        idxs = list()
+        for idx in self.train_batch_idxs:
+            idxs += range(idx*self.batch_size, (idx+1)*self.batch_size)        
+        datasets['train'] = Dataset.from_dict(datasets['train'][[idx for idx in idxs if idx < num_train]])
+
+        return datasets
+
+
+    def load_llm_preds(self, split):
+        labels = list()
+        rationales = list()
+        for idx in getattr(self, f'{split}_batch_idxs'):
+            with open(f'{self.data_root}/{self.dataset_name}/llm/{split}_CoT_{idx}.json') as f:
+                outputs = json.load(f)
+
+            for output in outputs:
+                rationale, label = self._parse_llm_output(output)
+
+                rationales.append(rationale)
+                labels.append(label)
+
+        return rationales, labels
+
+
+    def load_gpt_preds(self, split):
+        labels = list()
+        rationales = list()
+        
+        with open(f'{self.data_root}/gpt-neox/{self.dataset_name}/{split}.json') as f:
+            outputs = json.load(f)
+
+        for output in outputs:
+            rationale, label = self._parse_gpt_output(output)
+
+            rationales.append(rationale)
+            labels.append(label)
+
+        return rationales, labels
+
+
+    def _post_process(self, datasets):
+        raise NotImplementedError
+
+
+    def _parse_llm_output(self, output):
+        raise NotImplementedError
+
+
+    def _parse_gpt_output(self, output):
+        raise NotImplementedError
+      
+      
+def medical_meadow_medical_flashcards_data_loader(DatasetLoader):
+  dataset = load_dataset("medalpaca/medical_meadow_medical_flashcards")
+  pass 
---
+++ main.py
@@ -0,0 +1,43 @@
+import argparse
+from transformers import AutoTokenizer
+
+import data_utils
+
+
+# for i in range(5): 
+#     print(f"Sample {i+1}:")
+#     print(f"Input: {dataset['train'][i]['input']}")
+#     print(f"Instruction: {dataset['train'][i]['instruction']}")
+#     print(f"Output: {dataset['train'][i]['output']}\n")
+
+
+def use_standard_distillation(): 
+  def tokenize_function(examples):
+            model_inputs = tokenizer(
+                examples['input'],
+                max_length=args.max_input_length,
+                truncation=True
+            )
+
+            with tokenizer.as_target_tokenizer():
+                label_output_encodings = tokenizer(examples['label'], max_length=256, truncation=True)
+
+            model_inputs['labels'] = label_output_encodings['input_ids']
+
+            return model_inputs
+
+def run(args): 
+  pass 
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--from_pretrained', type=str, required=True)
+    parser.add_argument('--dataset', type=str, default='medalpaca/medical_meadow_medical_flashcards')
+    parser.add_argument('--model_type', type=str, default='standard')
+    parser.add_argument('--label_type', type=str, default='llm')
+    parser.add_argument('--batch_size', type=int, default=64)
+    
+    args = parser.parse_args()
+    run(args)
+  
